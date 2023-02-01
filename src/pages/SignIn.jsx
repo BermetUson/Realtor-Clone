827909de3247093c9f80a4a0872fb3a5ffce4eec
Bermet <oceaninapuddle@gmail.com>
2023-02-01 23:52:27 +0600

    fixed SignIn component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
 import Key from "../assets/key.jpg";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -27,13 +30,34 @@ const SignIn = () => {
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
             <input
-              className="w-full"
+              className="mb-6 w-full px-4 py-2 text-xl text-green-700 bg-white border-green-300 rounded transition ease-in-out"
               type="email"
               id="email"
               value={email}
               onChange={onChange}
               placeholder="Email address"
             />
+            <div className="relative mb-6">
+              <input
+                className="w-full px-4 py-2 text-xl text-green-700 bg-white border-green-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={onChange}
+                placeholder="Password"
+              />
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              ) : (
+                <AiFillEye
+                  className="absolute right-3 top-3 text-xl cursor-pointer"
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                />
+              )}
+            </div>
           </form>
         </div>
       </div>
