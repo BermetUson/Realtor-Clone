6f38b4f60b268a9034da0ec88f54ac4f7cd14f17
Bermet <oceaninapuddle@gmail.com>
2023-02-05 14:49:35 +0600

    create useAuthStatus hook

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,21 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+
+const useAuthStatus = () => {
+  const [loggedIn, setLoggenIn] = useState(false);
+  const [checkingStaus, setCheckingStaus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggenIn(true);
+      }
+      setCheckingStaus(false);
+    });
+  }, []);
+
+  return { loggedIn, checkingStaus };
+};
+
+export default useAuthStatus;
