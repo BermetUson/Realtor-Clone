2a9981e89ed9d3240c85ecf1e22c13eb1df7974a
Bermet <oceaninapuddle@gmail.com>
2023-02-10 22:08:54 +0600

    added style changes to ListingItem Profile

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,8 +5,8 @@ import { MdLocationPin } from "react-icons/md";
 
 const ListingItem = ({ listing, id }) => {
   return (
-    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150">
-      <Link to={`/category/${listing.type}/${id}`}>
+    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
         <img
           className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
           loading="lazy"
