aaedff7b68e23a73a3d3baca3669922ee73b31cf
Bermet <oceaninapuddle@gmail.com>
2023-02-12 20:58:28 +0600

    fixed Listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -13,6 +13,7 @@ import { db } from "../firebase";
 import "swiper/css/bundle";
 import { IoIosShareAlt } from "react-icons/io";
 import { MdLocationPin } from "react-icons/md";
+import { FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
@@ -93,11 +94,37 @@ const Listing = () => {
             {listing.address}
           </p>
           <div className="flex justify-start items-center space-x-4 w-[75%]">
-            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">{listing.type === "rent" ? "Rent" : "Sale"}</p>
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sale"}
+            </p>
             {listing.offer && (
-              <p className="bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">${+listing.regularPrice - +listing.discountedPrice} discount</p>
+              <p className="bg-green-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+                ${+listing.regularPrice - +listing.discountedPrice} discount
+              </p>
             )}
           </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold"> Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaParking className="text-lg mr-1" />
+              {listing.parking ? "Parking spot" : "No parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? "Furnished" : "Not furnished"}
+            </li>
+          </ul>
         </div>
         <div className="w-full h-[200px] lg-[400px] z-10 overflow-x-hidden "></div>
       </div>
