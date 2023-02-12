8dca7e75432333be1576d51f1a8fdcbae23e01a0
Bermet <oceaninapuddle@gmail.com>
2023-02-12 19:29:38 +0600

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,10 +11,12 @@ import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { db } from "../firebase";
 import "swiper/css/bundle";
+import { IoIosShareAlt } from "react-icons/io";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   const params = useParams();
 
@@ -55,6 +57,23 @@ const Listing = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center bg-opacity-80"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <IoIosShareAlt className="text-2xl text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-normal border-2 border-gray-400 rounded-full bg-white z-10 p-1 bg-opacity-50">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 };
